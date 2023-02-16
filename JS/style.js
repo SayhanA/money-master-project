710aa49d3ad742aea58ea82fe30d232a8aee0c8b
SayhanA <sayhanahmed05@gmail.com>
2023-02-16 13:08:06 +0600

    added code

--- JS/style.js
+++ JS/style.js
@@ -1,3 +1,40 @@
+function btnCalculate(idField){
+    const nameField = document.getElementById(idField);
+    const nameFieldValue = parseInt(nameField.value);
+    if(isNaN(nameFieldValue)){
+        alert('Please type a number')
+    }
+    else{
+        return nameFieldValue
+    }
+    return nameFieldValue
+}
+
 document.getElementById('btn-calculate').addEventListener('click',function(){
-    console.log('ok')
+    const incomeField = btnCalculate('input-field');
+    const foodField = btnCalculate('food-field');
+    const rentField = btnCalculate('rent-field');
+    const clothesField = btnCalculate('cloths-field');
+
+    
+
+    const expenseTotal = foodField + rentField + clothesField;
+    const remainingCost = incomeField - expenseTotal;
+    if(remainingCost>0){
+        document.getElementById('expenses').innerText = expenseTotal;
+        document.getElementById('balance').innerText = remainingCost;
+    }
+    return remainingCost;
 })
+
+function saveMoney(){
+    const saveField = btnCalculate('save-field');
+    if((saveField-100)<0){
+        const savedAmount = ((document.getElementById('balance').innerText) * saveField)/100;
+        document.getElementById('saving-amount').innerText = savedAmount;
+        document.getElementById('remaining-amount').innerText = (document.getElementById('balance').innerText) - savedAmount;
+    }
+    else{
+        alert('Please type a valid Percentage(%)')
+    }
+}
